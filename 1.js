03a7949cbd74412401747b347ae2b578af257878
= <=>
2020-01-30 14:30:07 +0700

    co ban xong file 1.js

---
+++ 1.js
@@ -0,0 +1,12 @@
+﻿ $(function(){
+ 	$(".noidungthem").slideUp(0);
+ 	 	TweenMax.staggerFrom($(".noidungbandau .to "), 0.5 ,{top:100,opacity:0},0.2)
+
+
+ 	$(".nutload").click(function(event) {
+ 		/* Act on the event */
+ 		$(".noidungthem").slideDown(200);
+ 		TweenMax.staggerFrom($(".noidungthem .to "), 0.5 ,{left:100,opacity:0},0.2)
+ 	});
+})  
+ 
